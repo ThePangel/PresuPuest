82bd1da76098d5051ffb26ca514b769d536aa7b4
ThePangel <74447121+ThePangel@users.noreply.github.com>
2024-07-01 23:46:44 +0200

    Added widgets for each entry of the database

--- expensetracker/lib/main.dart
+++ expensetracker/lib/main.dart
@@ -1,70 +1,63 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'dart:convert';
+
+
+List<Expense> _list = [];
 
 void main() async {
-  
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   final database = openDatabase(
-  
-  join(await getDatabasesPath(), 'expenses.db'),
-  onCreate: (db, version) {
-    
-    return db.execute(
-      'CREATE TABLE expenses(name TEXT PRIMARY KEY, cost INTEGER, image TEXT)',
-    );
-  },
-  
-  version: 1,
+    join(await getDatabasesPath(), 'expenses.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE expenses(name TEXT PRIMARY KEY, cost INTEGER, image TEXT)',
+      );
+    },
+    version: 1,
   );
 
   Future<void> insertExpense(Expense expense) async {
-  
-  final db = await database;
+    final db = await database;
 
-  
-  await db.insert(
-    'expenses',
-    expense.toMap(),
-    conflictAlgorithm: ConflictAlgorithm.replace,
+    await db.insert(
+      'expenses',
+      expense.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
-  
+
   Future<List<Expense>> retrieveExpenses() async {
-  
-  final db = await database;
+    final db = await database;
 
-  
-  final List<Map<String, Object?>> expenseMaps = await db.query('expenses');
+    final List<Map<String, Object?>> expenseMaps = await db.query('expenses');
 
-  
-  return [
-    for (final {
-          'name': name as String,
-          'cost': cost as int,
-          'image': image as String,
-        } in expenseMaps)
-      Expense(name: name, cost: cost, image: image),
+    return [
+      for (final {
+            'name': name as String,
+            'cost': cost as int,
+            'image': image as String,
+          } in expenseMaps)
+        Expense(name: name, cost: cost, image: image),
     ];
-  } 
-  Future<void> deleteExpense(String name) async {
-  
-  final db = await database;
+  }
 
-  
-  await db.delete(
-    'expenses',
-    
-    where: 'name = ?',
-    
-    whereArgs: [name],
-  );
-}
+  Future<void> deleteExpense(String name) async {
+    final db = await database;
+
+    await db.delete(
+      'expenses',
+      where: 'name = ?',
+      whereArgs: [name],
+    );
+  }
+
+  _list = await retrieveExpenses();
   runApp(const MyApp());
 }
 
@@ -94,14 +87,11 @@ class Expense {
     };
   }
 
-  // Implement toString to make it easier to see information about
-  // each dog when using the print statement.
   @override
   String toString() {
     return 'Expense{name: $name, cost: $cost, image: $image}';
   }
 }
-
 
 class _MyAppState extends State<MyApp> {
   int _selectedIndex = 0;
@@ -126,7 +116,30 @@ class _MyAppState extends State<MyApp> {
               ),
             ),
             body: <Widget>[
-              const Center(child: Text("Home Page")),
+              Column(
+                children: [
+                  Center(
+                      child: ListView.builder(
+                    itemCount: _list.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                            title: Text(_list[index].name),
+                            subtitle: Text(_list[index].cost.toString()),
+                            leading: Image.memory(
+                              base64Decode(_list[index].image),
+                              fit: BoxFit.cover,
+                              width: 100,
+                              height: 100,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            )),
+                      );
+                    },
+                  )),
+                ],
+              ),
               const Center(child: Text("2º page")),
             ][_selectedIndex],
             bottomNavigationBar: NavigationBar(
@@ -134,7 +147,6 @@ class _MyAppState extends State<MyApp> {
               onDestinationSelected: (int index) {
                 setState(() {
                   _selectedIndex = index;
-                  // prefs.setInt('page', index);
                 });
               },
               destinations: [
